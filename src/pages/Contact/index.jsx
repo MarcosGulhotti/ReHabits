f1cb8392dc4e203aaafcf0e710d8dcad0b79799e
marcos <marcosmvg77@gmail.com>
2021-08-12 16:51:14 -0300

    Feat: Page Contact quase pronta

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,62 @@
-import { Menu } from '../../components/Menu'
+import { Menu } from "../../components/Menu";
+import { ContactCard } from "../../components/ContactCard";
+import styled from "styled-components";
+
+const Dados = {
+  Marcos: {
+    name: "Marcos Vinicius Gulhotti",
+    img: "https://ca.slack-edge.com/TQZR39SET-U01PPAZEEPQ-5eed9ffeb4a2-512",
+    role: "Product Owner",
+    cellphone: "(45) 9 9912-7745",
+  },
+  Arthur: {
+    name: "Arthur Felini Linemburg",
+    img: "https://ca.slack-edge.com/TQZR39SET-U01SJTKSRRR-086832d3d4a5-512",
+    role: "Teach Leader",
+    cellphone: "(48) 9 9601-0693",
+  },
+  Lucas: {
+    name: "Lucas Cardoso",
+    img: "https://ca.slack-edge.com/TQZR39SET-U01T8FGTHR6-1050fc9757a2-512",
+    role: "Quality Assurance",
+    cellphone: "",
+  },
+  Luiz: {
+    name: "Luiz Victor B. Oliveira",
+    img: "https://ca.slack-edge.com/TQZR39SET-U01SCCZS11T-26bf72c9a4b0-512",
+    role: "Scrum Master",
+    cellphone: "(11) 9 8079-9716",
+  },
+};
+const Container = styled.div`
+  @media (min-width: 769px) {
+    height: 100vh;
+  }
+  width: 100%;
+  background-color: var(--background);
+`;
+const ContactDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  @media (min-width: 769px) {
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: row;
+  }
+  margin-top: 3rem;
+`;
 
 export const Contact = () => {
   return (
-    <Menu />
-  )
-}
+    <Container>
+      <Menu />
+      <ContactDiv>
+        <ContactCard Dados={Dados.Marcos} />
+        <ContactCard Dados={Dados.Arthur} />
+        <ContactCard Dados={Dados.Lucas} />
+        <ContactCard Dados={Dados.Luiz} />
+      </ContactDiv>
+    </Container>
+  );
+};
