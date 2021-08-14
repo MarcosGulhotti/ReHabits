f62a692b2c28cb82012f56a92abb9c27cf8620ea
marcos <marcosmvg77@gmail.com>
2021-08-14 10:45:35 -0300

    Feat: Links para contato colocados

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -8,36 +8,37 @@ const Dados = {
     img: "https://ca.slack-edge.com/TQZR39SET-U01PPAZEEPQ-5eed9ffeb4a2-512",
     role: "Product Owner",
     cellphone: "(45) 9 9912-7745",
-    linkedin: null,
-    gitlab: null,
-    github: null,
+    linkedin: "https://www.linkedin.com/in/marcos-vinicius-gulhotti-35b4571a1/",
+    gitlab: "https://gitlab.com/Markinn",
+    github: "https://github.com/MarcosGulhotti",
   },
   Arthur: {
     name: "Arthur Felini Linemburg",
     img: "https://ca.slack-edge.com/TQZR39SET-U01SJTKSRRR-086832d3d4a5-512",
     role: "Teach Leader",
     cellphone: "(48) 9 9601-0693",
-    linkedin: null,
-    gitlab: null,
-    github: null,
+    linkedin: "https://www.linkedin.com/in/arthur-fenili-linemburg-ab8936184/",
+    gitlab: "https://gitlab.com/tukinhafl",
+    github: "https://github.com/tukinhafl",
   },
   Lucas: {
     name: "Lucas Cardoso",
     img: "https://ca.slack-edge.com/TQZR39SET-U01T8FGTHR6-1050fc9757a2-512",
     role: "Quality Assurance",
-    cellphone: "",
-    linkedin: null,
-    gitlab: null,
-    github: null,
+    cellphone: "(41) 9 97099033",
+    linkedin: "https://www.linkedin.com/in/lucas-cardoso-78a19021a/",
+    gitlab: "https://gitlab.com/padocaking",
+    github: "https://github.com/padocaking",
   },
   Luiz: {
     name: "Luiz Victor B. Oliveira",
     img: "https://ca.slack-edge.com/TQZR39SET-U01SCCZS11T-26bf72c9a4b0-512",
     role: "Scrum Master",
     cellphone: "(11) 9 8079-9716",
-    linkedin: 'https://www.linkedin.com/in/luiz-victor-bispo-oliveira-bb6007170/',
-    gitlab: 'https://gitlab.com/LuizVictor19',
-    github: 'https://github.com/luizvictor19',
+    linkedin:
+      "https://www.linkedin.com/in/luiz-victor-bispo-oliveira-bb6007170/",
+    gitlab: "https://gitlab.com/LuizVictor19",
+    github: "https://github.com/luizvictor19",
   },
 };
 const Container = styled.div`
@@ -46,6 +47,7 @@ const Container = styled.div`
   }
   width: 100%;
   background-color: var(--background);
+  overflow: scroll;
 `;
 const ContactDiv = styled.div`
   display: flex;
@@ -60,14 +62,15 @@ const ContactDiv = styled.div`
 `;
 
 export const Contact = () => {
+  const { Marcos, Arthur, Lucas, Luiz } = Dados;
   return (
     <Container>
       <Menu />
       <ContactDiv>
-        <ContactCard Dados={Dados.Marcos} />
-        <ContactCard Dados={Dados.Arthur} />
-        <ContactCard Dados={Dados.Lucas} />
-        <ContactCard Dados={Dados.Luiz} />
+        <ContactCard Dados={Marcos} />
+        <ContactCard Dados={Arthur} />
+        <ContactCard Dados={Lucas} />
+        <ContactCard Dados={Luiz} />
       </ContactDiv>
     </Container>
   );
