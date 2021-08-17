de411311437edf18a1868428e588bbcd370875e8
marcos <marcosmvg77@gmail.com>
2021-08-17 11:50:34 -0300

    Feat: Logica para criar atividades feita

---
+++ src/components/FormActivitiesModal/index.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import { Input } from "../Input";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import api from "../../services/api";
+import { InputDate } from "../../components/InputDate";
+
+const Container = styled.div`
+  width: 510px;
+  height: 475px;
+  background-color: var(--background);
+  border-radius: 10px;
+  padding: 1rem;
+  margin-bottom: 8rem;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    div {
+      width: 100%;
+      margin-bottom: 1rem;
+    }
+  }
+`;
+
+export const FormActivitiesModal = ({
+  groupId,
+  setActivitiesModal,
+  setGroupActivities,
+  groupActivities,
+}) => {
+  const token = JSON.parse(localStorage.getItem("token"))
+  const formSchema = yup.object().shape({
+    title: yup.string().required("Campo obrigatório"),
+    realization_time: yup.string().required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const formSubmit = async (data) => {
+    const newData = { ...data, group: groupId };
+
+   await api
+      .post("activities/", newData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit(formSubmit)}>
+        <div className="inputDiv">
+          <Input
+            error={errors.title?.message}
+            name="title"
+            register={register}
+            placeholder="Coloque o titulo aqui"
+            label="Titulo"
+          />
+        </div>
+        <div className="inputDiv">
+          <InputDate
+            error={errors.realization_time?.message}
+            name="realization_time"
+            register={register}
+            placeholder="Coloque o tempo de realização aqui"
+            label="Tempo de realização"
+            type="date"
+          />
+        </div>
+        <button type="submit">Criar atividade</button>
+      </form>
+    </Container>
+  );
+};
--- src/pages/SpecifyGroup/index.jsx
+++ src/pages/SpecifyGroup/index.jsx
@@ -7,23 +7,25 @@ import { StyledGoalsActivitiesList } from "../../components/GroupList";
 import { StyledCardGoals } from "../../components/CardGroup";
 import { CheckBox, CheckBoxDiv, InfosDiv } from "../../components/CardGroup";
 import { FormGoalsModal } from "../../components/FormGoalsModal";
-import { Modal, ButtonPosition } from '../../components/ModalHabit';
+import { Modal, ButtonPosition } from "../../components/ModalHabit";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Input } from '../../components/Input'
+import { Input } from "../../components/Input";
 import { useUserId } from "../../providers/UserId";
+import { FormActivitiesModal } from "../../components/FormActivitiesModal";
 
 export const SpecifyGroup = () => {
   const { id } = useParams();
-  const token = JSON.parse(localStorage.getItem("token"))
+  const token = JSON.parse(localStorage.getItem("token"));
   const [dataGroup, setDataGroup] = useState([]);
   const history = useHistory();
   const [groupGoals, setGroupGoals] = useState([]);
   const [groupActivities, setGroupActivities] = useState([]);
   const [goalModal, setgoalModal] = useState(false);
-  const [editGroupModal, setEditGroupModal] = useState(false)
-  const userInfo = useUserId()
+  const [activitiesModal, setActivitiesModal] = useState(false);
+  const [editGroupModal, setEditGroupModal] = useState(false);
+  const userInfo = useUserId();
 
   const handleSubscribe = async (id) => {
     await api.post(
@@ -75,7 +77,7 @@ export const SpecifyGroup = () => {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then(() => setGroupGoals(groupGoals.filter(elm => elm.id !== id)))
+      .then(() => setGroupGoals(groupGoals.filter((elm) => elm.id !== id)))
       .catch((e) => console.log(e));
   };
 
@@ -84,31 +86,38 @@ export const SpecifyGroup = () => {
   });
 
   const {
-      register,
-      handleSubmit,
-      formState: { errors },
+    register,
+    handleSubmit,
+    formState: { errors },
   } = useForm({
-      resolver: yupResolver(formSchema),
+    resolver: yupResolver(formSchema),
   });
 
   const handleEditGroup = async (data) => {
     if (dataGroup.creator.id === userInfo.id) {
       await api.patch(`/groups/${Number(id)}/`, data, {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-      await alert('grupo editado')
-      .catch(() => alert('Alguma coisa deu errada.'))
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      await alert("grupo editado").catch(() =>
+        alert("Alguma coisa deu errada.")
+      );
     } else {
-      alert('Apenas o dono do grupo pode editar.')
+      alert("Apenas o dono do grupo pode editar.");
     }
   };
 
   return (
     <>
       <Menu />
-      <StyledBackgroundGroups style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <StyledBackgroundGroups
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
         <div id="headerPositionSpecify">
           <button
             onClick={() =>
@@ -168,20 +177,65 @@ export const SpecifyGroup = () => {
             </>
           )}
         </div>
-        <div className='goalsActivitiesContainerList'>
+        <div className="goalsActivitiesContainerList">
           <h2>Atividades</h2>
+          {activitiesModal ? (
+            <FormActivitiesModal
+              groupId={id}
+              setActivitiesModal={setActivitiesModal}
+              setGroupActivities={setGroupActivities}
+              groupActivities={groupActivities}
+            />
+          ) : (
+            <StyledGoalsActivitiesList>
+              {groupActivities.map((el) => (
+                <StyledCardGoals style={{ height: `200px` }} key={el.id}>
+                  {console.log(groupActivities)}
+                  <CheckBoxDiv>
+                    <h1>{el.title}</h1>
+                    <i onClick={() => null} class="fas fa-minus-circle" />
+                  </CheckBoxDiv>
+                  <InfosDiv>
+                    <button
+                      style={{
+                        height: `35px`,
+                        backgroundColor: `var(--background)`,
+                        color: `var(--white)`,
+                        margin: `0 auto`,
+                      }}
+                    >
+                      Editar
+                    </button>
+                  </InfosDiv>
+                </StyledCardGoals>
+              ))}
+            </StyledGoalsActivitiesList>
+          )}
+          <button
+            onClick={() => setActivitiesModal(!activitiesModal)}
+            className="createGoalsButton"
+          >
+            Criar Atividade
+          </button>
         </div>
-        <button id='editGroup' onClick={() => setEditGroupModal(true)}>
+        <button id="editGroup" onClick={() => setEditGroupModal(true)}>
           <i class="fas fa-edit"></i>
         </button>
-        {editGroupModal && 
-          <Modal style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        {editGroupModal && (
+          <Modal
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
             <form onSubmit={handleSubmit(handleEditGroup)}>
-            <i
-              onClick={() => setEditGroupModal(false)}
-              class="fas fa-chevron-left"
-              id="return"
-            />
+              <i
+                onClick={() => setEditGroupModal(false)}
+                class="fas fa-chevron-left"
+                id="return"
+              />
               <div>
                 <div>
                   <Input
@@ -200,7 +254,7 @@ export const SpecifyGroup = () => {
               </ButtonPosition>
             </form>
           </Modal>
-        }
+        )}
       </StyledBackgroundGroups>
     </>
   );
